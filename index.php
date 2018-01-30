224df488311843f44c1178018cab0d19b13735b0
Nathan Bullen <nathanbullen6@gmail.com>
2018-01-30 17:18:51 +1100

    Mock initialised, header menu mocked.

---
+++ index.php
@@ -0,0 +1,13 @@
+<head>
+  <link rel="stylesheet" href="css/style.css?v=<?=time();?>">
+</head>
+<body>
+  <div id="menu">
+    <div id="logo">
+      <img class="imagelogo" src="https://www.connecteducation.com.au/assets/logo_black-97c14e77ecec2c4624b560c20c53df78b1d81159eb0fd1f2ca7040f4e289a209.png" alt="Connect App"></img>
+    </div>
+    <div id="login">
+      <img class="profile" src="https://www.mathlearningcenter.org/sites/default/files/images/profile-blank.png" alt="Profile Picture"></img>
+    </div>
+  </div>
+</body>
