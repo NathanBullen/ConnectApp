edc6a5f3b237d51063107e41a6929bbd52a69419
Nathan Bullen <nathanbullen6@gmail.com>
2018-01-30 17:47:49 +1100

    Started side menu

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <head>
-  <link rel="stylesheet" href="css/style.css?v=<?=time();?>">
+    <link rel="stylesheet" href="css/style.css?v=<?=time();?>">
 </head>
 <body>
-  <div id="menu">
+  <div id="header">
     <div id="logo">
       <img class="imagelogo" src="https://www.connecteducation.com.au/assets/logo_black-97c14e77ecec2c4624b560c20c53df78b1d81159eb0fd1f2ca7040f4e289a209.png" alt="Connect App"></img>
     </div>
@@ -10,4 +10,14 @@
       <img class="profile" src="https://www.mathlearningcenter.org/sites/default/files/images/profile-blank.png" alt="Profile Picture"></img>
     </div>
   </div>
+<div id="menu">
+  <div id="home">
+    <i class="fa fa-home lefticon" aria-hidden="true"></i>Home
+  </div>
+  <div id="topics">
+    <i class="fa fa-list-ul lefticon" aria-hidden="true"></i>Topics<i class="fa fa-angle-right righticon" aria-hidden="true"></i>
+  </div>
+  <div id="pratice-quiz">
+  </div>
+</div>
 </body>
