217108be6a9fe97fd729a9c3b65308d9d5661797
Nathan Bullen <nathanbullen6@gmail.com>
2018-02-01 15:58:14 +1100

    Removed illegal token

--- src/data/QuizQuestions.js
+++ src/data/QuizQuestions.js
@@ -2,7 +2,7 @@ var quizQuestions = [
   {
       question: "Breathalysers are fuel cells used to measure the level of alcohol (ethanol) in a person's breath. The fuel cell uses oxygen gas from the air to oxidise ethanol, C2HH5OH, in the breath to ethanoic acid, CH3COOH. The cell uses phosphoric acid, H3PO4, as the electrolyte, and the only product is water.\\n\\nThe reaction occuring at the negative electrode is:",
       answers: ['2CH3CH2OH(g) + O2(g) -> 2CH3COOH(aq) + 4H+(aq) + 4e-', 'CH3CH2OH(g) + H2O(l) -> CH3COOH(aq) + 4H+ (aq) + 4e-', 'O2(g) + 2H20(l) + 4e- -> 4OH-(aq)', 'O2(g) + 4H+(aq) + 4e- -> 2H2O(l)'],
-      correct-answer: 3
+      correctanswer: 3
   }
 ];
 
