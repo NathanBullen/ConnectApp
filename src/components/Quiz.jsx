ba01c0f08db5be84172b49f183de49f5740abc67
Nathan Bullen <nathanbullen6@gmail.com>
2018-02-02 11:01:52 +1100

    Added starting overlay of the Quiz and Quiz back end; derived from https://github.com/florinpop17/quiz-app/blob/master/app/components/Main.jsx

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,20 +1,77 @@
 import React from 'react';
-import logo from '../logo.svg';
+import data from '../data/QuizQuestions.js';
+import QuizOverlay from './QuizOverlay';
 
-function Menu() {
-    return (
-  		<div id="quiz">
-			<div className="App">
-				<header className="App-header">
-				  <img src={logo} className="App-logo" alt="logo" />
-				  <h1 className="App-title">Connect Education Quiz</h1>
-				</header>
-				<p className="App-intro">
-				  Main component src/components/Quiz.jsx
-				</p>
-			  </div>
-		  </div>
-    );
-}
+class Quiz extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentQuestion: 0,
+            total: data.length,
+            questionAnswered: false,
+            score: 0,
+			displayEndQuiz: 'flex'
+        }
+        this.nextQuestion = this.nextQuestion.bind(this);
+        this.handleStartQuiz = this.handleStartQuiz.bind(this);
+        this.handleIncreaseScore = this.handleIncreaseScore.bind(this);
+    }
 
-export default Menu;
+    loadQuestion(currentQuestion) {
+        this.setState({
+            question: data[currentQuestion].question,
+            answers: [data[currentQuestion].answers],
+            correctanswer: data[currentQuestion].correctanswer,
+            currentQuestion: currentQuestion + 1
+        });
+    }
+
+    componentWillMount() {
+        let { currentQuestion } = this.state;
+        this.loadQuestion(currentQuestion);
+    }
+
+    nextQuestion() {
+        let { currentQuestion, total, score } = this.state;
+
+        if(currentQuestion === total){
+			//End Quiz State
+            this.setState({
+                displayEndQuiz: 'flex'
+            });
+        } else {
+            this.loadQuestion(currentQuestion);
+            this.setState({
+                questionAnswered: false
+            });
+        }
+
+    }
+
+    handleStartQuiz() {
+        this.setState({
+            displayEndQuiz: 'none',
+            currentQuestion: 1
+        });
+    }
+
+    handleIncreaseScore() {
+        this.setState({
+            score: this.state.score + 1
+        });
+    }
+
+    render() {
+        let { currentQuestion, total, question, answers, correct, questionAnswered, displayEndQuiz, score} = this.state;
+
+        return (
+            <div id="quiz">
+			
+			<QuizOverlay style={{display: displayEndQuiz}} score={score} total={total} startQuiz={this.handleStartQuiz}/>
+			
+            </div>
+        );
+    }
+};
+
+export default Quiz
---
+++ src/components/QuizOverlay.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+
+class QuizOverlay extends React.Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            finished: false,
+            title: 'Quiz',
+            description: 'Practice quiz, blah blah blah blah' ,
+            buttonText: 'Start the quiz' 
+        };
+        
+        this.popupHandle = this.popupHandle.bind(this);
+    }
+    
+    popupHandle() {
+        let { finished } = this.state;
+        
+        if(!finished){
+            this.setState({
+                finished: true,
+                title: 'Congratulations!',
+                buttonText: 'Restart'
+            });
+            
+            this.props.startQuiz();
+        } else {            
+            window.location.reload();// restart the application
+        }
+    }
+    
+    componentWillReceiveProps(nextProps) {
+        this.setState({
+            description: 'You have completed the quiz. <br /> You got: <strong>' + this.props.score + '</strong> out of <strong>' +this.props.total +'</strong> questions right.'
+        })
+    }
+    
+    createMarkup(html) {
+        return {__html: html};
+    }
+    
+    
+    render() {
+       
+        let { title, description, buttonText } = this.state;
+        
+        let { style } = this.props;
+        
+        return (
+            <div class="quiz-introduction" style={style}>
+				<div class="quiz-introduction-holder">
+					<div class="quiz-introduction-text-box">
+						<div class="rounded-box">
+							<h1>{title}</h1>
+						</div>
+						<p dangerouslySetInnerHTML={this.createMarkup(description)} />
+					</div>
+					<div class="horizontal-line"></div>
+					<button className="fancy-btn" onClick={this.popupHandle}>{buttonText}</button>
+					<div class="horizontal-line"></div>
+				</div>
+            </div>
+        );
+    }
+}
+
+export default QuizOverlay
