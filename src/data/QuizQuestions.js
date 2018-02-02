385f122d747f6f77ebcd67613fc60aa5d318b67e
Nathan Bullen <nathanbullen6@gmail.com>
2018-02-02 14:57:35 +1100

    Adding long question to test

--- src/data/QuizQuestions.js
+++ src/data/QuizQuestions.js
@@ -3,6 +3,11 @@ var quizQuestions = [
       question: "Breathalysers are fuel cells used to measure the level of alcohol (ethanol) in a person's breath. The fuel cell uses oxygen gas from the air to oxidise ethanol, C2HH5OH, in the breath to ethanoic acid, CH3COOH. The cell uses phosphoric acid, H3PO4, as the electrolyte, and the only product is water.\\n\\nThe reaction occuring at the negative electrode is:",
       answers: ['2CH3CH2OH(g) + O2(g) -> 2CH3COOH(aq) + 4H+(aq) + 4e-', 'CH3CH2OH(g) + H2O(l) -> CH3COOH(aq) + 4H+ (aq) + 4e-', 'O2(g) + 2H20(l) + 4e- -> 4OH-(aq)', 'O2(g) + 4H+(aq) + 4e- -> 2H2O(l)'],
       correctanswer: 3
+  },
+  {
+	  question:"Which answer is the longest?",
+	  answers: ['Not this one', 'Probably not this one' , 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'Ugh'],
+	  correctanswer: 3
   }
 ];
 
